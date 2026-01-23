e3e110c725670f457dd7f4d1a4efc05c6baf7bb3
sejjy <sejjymvm@gmail.com>
2026-01-23 12:40:48 +0800

    improve comment readability

--- ch-2/src/4.c
+++ ch-2/src/4.c
@@ -1,5 +1,6 @@
 // Write a program that asks the user to enter a dollars-and-cents amount, then displays the
 // amount with 5% tax added:
+//
 // Enter an amount: 100.00
 // With tax added: $105.00
 
--- ch-2/src/6.c
+++ ch-2/src/6.c
@@ -1,6 +1,8 @@
 // Modify the program of Programming Project 5 so that the polynomial is evaluated using the
 // following formula:
+//
 // ((((3x + 2)x – 5)x – 1)x + 7)x – 6
+//
 // Note that the modified program performs fewer multiplications. This technique for evaluat-
 // ing polynomials is known as Horner’s Rule.
 
